ddec40c9fdef0d60dabac6dee168bc5ec7c51e72
King Ankoue <thierryankoue@pursuit.com>
2022-08-30 21:24:32 -0400

    heroku deployment

--- back-end/db/dbConfig.js
+++ back-end/db/dbConfig.js
@@ -18,7 +18,7 @@ const cn = DATABASE_URL
       port: PG_PORT,
       database: PG_DATABASE,
       user: PG_USER,
-      // password: PG_PASSWORD,
+      password: PG_PASSWORD,
     };
 
 const db = pgp(cn);

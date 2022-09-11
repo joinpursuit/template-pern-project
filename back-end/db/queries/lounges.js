c7f94acf756125371a81d6ae9d48b97c23afa067
King Ankoue <thierryankoue@pursuit.com>
2022-09-11 16:45:04 -0400

    update database3

--- back-end/db/queries/lounges.js
+++ back-end/db/queries/lounges.js
@@ -46,19 +46,13 @@ const addLounge = async (req, res) => {
 
 const editLounge = async (req, res) => {
   try {
-    let {photos} = req.body.photos;
-    let {borough} = req.body.borough;
-    let {zip_code} = req.body.zip_code;
-    let {lounge_name} = req.body.lounge_name;
-    let {Phone_Number} = req.body.phone_number;
     let {Days_Closed} = req.body.days_closed;
-    let {Street_Address} = req.body.street_address;
     let {Serves_Hookah} = req.body.serves_hookah;
     let {id} = req.params.id;
     
     let editLounge = await db.any(
-        `UPDATE lounges SET Photos = $Photos, Borough = $Borough, Zip_Code = $Zip_Code, Lounge_Name = $Lounge_Name, Phone_Number = $Phone_Number, Days_Closed = $Days_Closed, Street_Address = $Street_Address, Serves_Hookah= $Serves_Hookah Photos=$Photos WHERE id = $9 RETURNING *`,
-        [photos, borough, zip_code, lounge_name, Phone_Number, Days_Closed, Street_Address, Serves_Hookah, id]
+        `UPDATE lounges SET Days_Closed = $Days_Closed, Serves_Hookah= $Serves_Hookah WHERE id=${id} RETURNING *`,
+        [Days_Closed, Serves_Hookah, id]
       );
     res.json(getLounges);
     return editLounge;

329928facc71280cffce184b77f403f8fccc4fb7
King Ankoue <thierryankoue@pursuit.com>
2022-09-14 17:51:12 -0400

    FINITO

--- front-end/src/Components/Lounges.js
+++ front-end/src/Components/Lounges.js
@@ -6,6 +6,7 @@ import Row from "react-bootstrap/Row";
 
 
 // const API = "http://localhost:3333"; // should use Heroku/ netlify link
+const API2 = "https://warm-ravine-05696.herokuapp.com"
 const API = process.env.REACT_APP_API_URL;
 
 function Lounges() {
@@ -13,11 +14,12 @@ function Lounges() {
   
   useEffect(() => {
     axios
-    .get(`${API}/lounges`)
+    .get(`${API2}/lounges`)
     .then((res) => setLounges(res.data))
     .catch((err) => console.error(err));
   }, []);
-  
+console.log("lounges=", lounges)
+
   return (
       <Container fluid>
         <Row className="g-2">
